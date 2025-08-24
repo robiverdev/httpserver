69d08eb464cdeb8dcd45f2a03cc52dcdc585d418
robiverdev <robi.verdev1@gmail.com>
2025-08-24 16:47:46 +0200

    Added Server struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1".to_string());
+    server.run();
+}
+
+struct Server {
+    address: String,
+}
+impl Server {
+    fn new(address: String) -> Self {
+        Self { address }
+    }
+
+    fn run(self) {
+        println!("Server is listening on {}", self.address)
+    }
 }
