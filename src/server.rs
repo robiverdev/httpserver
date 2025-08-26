b51836a7b82e2d9d1b7ce0398a6b289969e5516b
robiverdev <robi.verdev1@gmail.com>
2025-08-26 11:26:54 +0200

    add loop to accept incoming connections and logs result

--- src/server.rs
+++ src/server.rs
@@ -3,6 +3,7 @@ use std::net::TcpListener;
 pub struct Server {
     address: String,
 }
+
 impl Server {
     pub fn new(address: String) -> Self {
         Self { address }
@@ -12,5 +13,16 @@ impl Server {
         println!("Server is listening on {}", self.address);
 
         let listener = TcpListener::bind(&self.address).unwrap();
+
+        // Infinite loop
+        loop {
+            match listener.accept() {
+                Ok((stream, _)) => {
+                    let a = 5;
+                    println!("OK")
+                }
+                Err(e) => println!("Failed to connect. Error: {}", e),
+            }
+        }
     }
 }
