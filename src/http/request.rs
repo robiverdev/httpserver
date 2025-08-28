d11a72066c060d8e6fd151eb6c18626330de913a
robiverdev <robi.verdev1@gmail.com>
2025-08-28 11:38:02 +0200

    add error handling traits for ParseError

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,17 +1,68 @@
-use std::convert::TryFrom;
-use std::fmt::Error;
-
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
+use std::str::Utf8Error;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
-    pub method: Method,
+    method: Method,
 }
 
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+    type Error = ParseError;
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+        // match str::from_utf8(buf) {
+        //     Ok(request) => {}
+        //     Err(_) => return Err(ParseError::InvalidEncoding),
+        // }
+
+        // match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
+        //     Ok(request) => {}
+        //     Err(e) => return Err(e),
+        // }
+        // ? -> basically the same as the match.
+        let request = str::from_utf8(buf)?; // ? will try to conver the error type it recieves if it does not match the error type the fn is suppose to return
         unimplemented!();
     }
 }
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message());
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message());
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEncoding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method",
+        }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
+impl Error for ParseError {}
